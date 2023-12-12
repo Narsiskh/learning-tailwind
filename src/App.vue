5fbef04ce88361b398a42c9746ee1dc356e6c052
Narsis <narsiskh.2013@gmail.com>
2023-12-12 21:11:56 +0330

    learning about typography

--- src/App.vue
+++ src/App.vue
@@ -3,14 +3,24 @@
 
 <template>
   <main>
-  <h1 class="border-4 border-green-600 text-vueJs-932">
-    Hello world!
-  </h1>
-  <h1 class="bg-yellow-600 text-white">
-    Hello world!
-  </h1>
-    <p>Hello long sentence</p>
-    <p>Another long sentence</p>
+    <div>
+      <h1 class="border-4 border-green-600 text-vueJs-932">
+        Hello world!
+      </h1>
+      <h1 class="bg-yellow-600 text-white">
+        Hello world!
+      </h1>
+      <p>Hello long sentence</p>
+      <p>Another long sentence</p>
+    </div>
+    <div>
+      <h1>Title 1</h1>
+      <h2>Title 2</h2>
+      <h3>Title 3</h3>
+      <p>A regular paragraph</p>
+      <p>A description paragraph</p>
+      <p class="note">A little note</p>
+    </div>
   </main>
 </template>
 
